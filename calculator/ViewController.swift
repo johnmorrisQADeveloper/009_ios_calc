0ca6ee6d0452644674e5eb36f57344cdca8f11e4
john morris <fennymorris@live.co.uk>
2020-04-07 20:46:49 +0100

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by John Morris on 07/04/2020.
+//  Copyright © 2020 John Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
